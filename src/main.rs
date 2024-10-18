fc95927624de1dd31b4cb067ff0de5bd5eb3cbdb
Hamed <hamed.2012.darzi@gmail.com>
2024-10-18 23:42:11 +0200

    add while loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+/*
 fn main() {
     let mut a = 0;
     loop{
@@ -6,5 +7,13 @@ fn main() {
         }
         println!("{:?}", a);
         a = a+1;
+    }
+}
+*/
+fn main(){
+    let mut a = 0;
+    while a != 5{
+        println!("{:?}", a);
+        a = a + 1;
     }
 }
